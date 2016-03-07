71c63ebba0ed56df128c023d8026803dac71e3c7
mobinityfx <mobinity.fx@gmail.com>
2016-03-08 02:37:14 +0700

    Dashboard

---
+++ application/views/admin/isi/log_squid.php
@@ -0,0 +1,85 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Log 
+        <small>Squid Proxy</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+    <section class="content">
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-lg-12 col-xs-12">
+          <div class="box-body">
+            <table id="example1" class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>Rendering engine</th>
+                  <th>Browser</th>
+                  <th>Platform(s)</th>
+                  <th>Engine version</th>
+                  <th>CSS grade</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Trident</td>
+                  <td>Internet Explorer 4.0</td>
+                  <td>Win 95+</td>
+                  <td> 4</td>
+                  <td>X</td>
+                </tr>
+                 <tr>
+                        <td>Gecko</td>
+                        <td>Seamonkey 1.1</td>
+                        <td>Win 98+ / OSX.2+</td>
+                        <td>1.8</td>
+                        <td>A</td>
+                      </tr>
+                      <tr>
+                        <td>Gecko</td>
+                        <td>Epiphany 2.20</td>
+                        <td>Gnome</td>
+                        <td>1.8</td>
+                        <td>A</td>
+                      </tr>
+                      <tr>
+                        <td>Webkit</td>
+                        <td>Safari 1.2</td>
+                        <td>OSX.3</td>
+                        <td>125.5</td>
+                        <td>A</td>
+                      </tr>
+                      <tr>
+                        <td>Misc</td>
+                        <td>IE Mobile</td>
+                        <td>Windows Mobile 6</td>
+                        <td>-</td>
+                        <td>C</td>
+                      </tr>
+                      <tr>
+                        <td>Misc</td>
+                        <td>PSP browser</td>
+                        <td>PSP</td>
+                        <td>-</td>
+                        <td>C</td>
+                      </tr>
+                      <tr>
+                        <td>Other browsers</td>
+                        <td>All others</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>U</td>
+                      </tr>
+              </tbody>
+            </table>
+        </div>
+      </div>
+    </section>
+</div><!-- /.content-wrapper -->
+
